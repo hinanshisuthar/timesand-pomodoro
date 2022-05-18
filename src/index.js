46676e54a1d290f5f7a2bbb801b475e031c169da
Hinanshi Suthar <hinanshis4@gmail.com>
2022-05-18 14:49:48 +0530

    feat: data provider

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/auth-context";
+import { DataProvider } from "./context/data-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <DataProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </DataProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
